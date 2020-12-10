fde0f049caa7156e710d143702d36d158020608b
EvilKanoa <kanoa@kanoa.ca>
2020-12-10 17:44:37 -0500

    Added rest of content for q2

--- src/components/questionTwo/QuestionTwo.js
+++ src/components/questionTwo/QuestionTwo.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import PageContainer, { Section, Image } from '../pageContainer/PageContainer';
 
-import './QuestionTwo.css';
+const Reference = ({ id, text }) => (
+  <div style={{ paddingBottom: '30px', paddingLeft: '30px' }}>
+    <span style={{ marginLeft: '-30px' }}>[{id}]</span>
+    <span style={{ paddingLeft: '30px' }}>
+      <em>{text}</em>
+    </span>
+  </div>
+);
 
 const QuestionTwo = () => (
   <PageContainer
@@ -85,7 +92,7 @@ const QuestionTwo = () => (
       <blockquote>
         <p>
           "The Paris Agreement requests each country to outline and communicate their
-          post-2020 climate actions, known as their NDCs."
+          post-2020 climate actions, known as their NDCs."<sup>[1][2]</sup>
         </p>
       </blockquote>
 
@@ -128,21 +135,27 @@ const QuestionTwo = () => (
         </thead>
         <tbody>
           <tr>
-            <td>USA</td>
+            <td>
+              USA<sup>[3]</sup>
+            </td>
             <td>2005</td>
             <td>26%</td>
             <td>5,744</td>
             <td>2025</td>
           </tr>
           <tr>
-            <td>Canada</td>
+            <td>
+              Canada<sup>[4]</sup>
+            </td>
             <td>2005</td>
             <td>30%</td>
             <td>523</td>
             <td>2030</td>
           </tr>
           <tr>
-            <td>Mexico</td>
+            <td>
+              Mexico<sup>[5]</sup>
+            </td>
             <td>2013</td>
             <td>22%</td>
             <td>763</td>
@@ -163,21 +176,27 @@ const QuestionTwo = () => (
         year.
       </p>
 
-      <h4>United States of America's Prediction</h4>
+      <h4>
+        United States of America's Prediction<sup>[6]</sup>
+      </h4>
       <Image src="./images/question-two/CAT_Predictions_USA_2019Sep.PNG" />
       <p>
         Predicted CO2 eq. emissions by 2025 (target year):{' '}
         <strong>6,273&nbsp;Mt&nbsp;-&nbsp;6,373&nbsp;Mt</strong>
       </p>
 
-      <h4>Canada's Prediction</h4>
+      <h4>
+        Canada's Prediction<sup>[7]</sup>
+      </h4>
       <Image src="./images/question-two/CAT_Predictions_Canada_2019Sep.PNG" />
       <p>
         Predicted CO2 eq. emissions by 2030 (target year):{' '}
         <strong>630&nbsp;Mt&nbsp;-&nbsp;763&nbsp;Mt</strong>
       </p>
 
-      <h4>Mexico's Prediction</h4>
+      <h4>
+        Mexico's Prediction<sup>[8]</sup>
+      </h4>
       <Image src="./images/question-two/CAT_Predictions_Mexico_2019Sep.PNG" />
       <p>
         Predicted CO2 eq. emissions by 2030 (target year):{' '}
@@ -197,21 +216,27 @@ const QuestionTwo = () => (
         country's target year.
       </p>
 
-      <h4>United States of America's Prediction</h4>
+      <h4>
+        United States of America's Prediction<sup>[9]</sup>
+      </h4>
       <Image src="./images/question-two/CAT_Predictions_USA_2020Jul.PNG" />
       <p>
         Predicted CO2 eq. emissions by 2025 (target year):{' '}
         <strong>5,744&nbsp;Mt&nbsp;-&nbsp;6,135&nbsp;Mt</strong>
       </p>
 
-      <h4>Canada's Prediction</h4>
+      <h4>
+        Canada's Prediction<sup>[10]</sup>
+      </h4>
       <Image src="./images/question-two/CAT_Predictions_Canada_2020Sep.PNG" />
       <p>
         Predicted CO2 eq. emissions by 2030 (target year):{' '}
         <strong>603&nbsp;Mt&nbsp;-&nbsp;630&nbsp;Mt</strong>
       </p>
 
-      <h4>Mexico's Prediction</h4>
+      <h4>
+        Mexico's Prediction<sup>[11]</sup>
+      </h4>
       <Image src="./images/question-two/CAT_Predictions_Mexico_2020Sep.PNG" />
       <p>
         Predicted CO2 eq. emissions by 2030 (target year):{' '}
@@ -220,7 +245,109 @@ const QuestionTwo = () => (
     </Section>
 
     <Section title="Calculations & Results">
+      <p>
+        The below table outlines the predicted megatonnes of C02 eq. emissions pre- and
+        post-COVID for each of the countries, and the total for the three countries.
+        Additionally, we calculate the percent difference in predicted greenhouse gas
+        emissions for each country and for all three countries.
+      </p>
+      <table>
+        <thead>
+          <th>Country</th>
+          <th>Pre-COVID</th>
+          <th>Post-COVID</th>
+          <th>Percent Difference</th>
+          <th>Target</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>USA</td>
+            <td>6,273&nbsp;Mt&nbsp;-&nbsp;6,373&nbsp;Mt</td>
+            <td>5,744&nbsp;Mt&nbsp;-&nbsp;6,135&nbsp;Mt</td>
+            <td>-8.80%</td>
+            <td>5,744&nbsp;Mt</td>
+          </tr>
+          <tr>
+            <td>Canada</td>
+            <td>630&nbsp;Mt&nbsp;-&nbsp;763&nbsp;Mt</td>
+            <td>603&nbsp;Mt&nbsp;-&nbsp;630&nbsp;Mt</td>
+            <td>-4.38%</td>
+            <td>523&nbsp;Mt</td>
+          </tr>
+          <tr>
+            <td>Mexico</td>
+            <td>803&nbsp;Mt&nbsp;-&nbsp;843&nbsp;Mt</td>
+            <td>774&nbsp;Mt&nbsp;-&nbsp;852&nbsp;Mt</td>
+            <td>-3.68%</td>
+            <td>763&nbsp;Mt</td>
+          </tr>
+          <tr style={{ fontWeight: 'bold' }}>
+            <td>Total</td>
+            <td>7,706&nbsp;Mt&nbsp;-&nbsp;7,979&nbsp;Mt</td>
+            <td>7,121&nbsp;Mt&nbsp;-&nbsp;7,617&nbsp;Mt</td>
+            <td>-7.89%</td>
+            <td>7,030&nbsp;Mt</td>
+          </tr>
+        </tbody>
+      </table>
+    </Section>
 
+    <Section title="Conclusion">
+      <p>
+        From the percent difference in predicted CO2 emissions when comparing pre-COVID to
+        post-COVID, we can see that the effects of COVID-19 have indeed assisted North
+        America in hitting their emissions targets. Despite these effects though, we can
+        see from the total predicted greenhouse gas emissions that North America is still
+        predicated to be unable to hit their unconditional contributions as laid out in
+        their NDCs.
+      </p>
+    </Section>
+
+    <Section title="References">
+      <Reference
+        id={1}
+        text="United Nations. (n.d.). Nationally Determined Contributions (NDCs). Retrieved November 28, 2020, from https://unfccc.int/process-and-meetings/the-paris-agreement/the-paris-agreement/nationally-determined-contributions-ndcs"
+      />
+      <Reference
+        id={2}
+        text='The Paris Agreement". United Nations Treaty Collection. (2016, July 8). Retrieved November 28, 2020, from https://unfccc.int/process-and-meetings/the-paris-agreement/the-paris-agreement'
+      />
+      <Reference
+        id={3}
+        text="USA. (2016, September 2). USA First NDC. Retrieved November 28, 2020, from https://www4.unfccc.int/sites/NDCStaging/Pages/Party.aspx?party=USA"
+      />
+      <Reference
+        id={4}
+        text="Canada. (2017, May 10). Canada First NDC (Updated submission) (English). Retrieved November 28, 2020, from https://www4.unfccc.int/sites/NDCStaging/Pages/Party.aspx?party=CAN"
+      />
+      <Reference
+        id={5}
+        text="Mexico. (2016, September 20). Mexico First NDC. Retrieved November 28, 2020, from https://www4.unfccc.int/sites/NDCStaging/Pages/Party.aspx?party=MEX​"
+      />
+      <Reference
+        id={6}
+        text="Climate Action Tracker. (2019, September 19). Climate Action Tracker | USA. Retrieved November 28, 2020, from https://climateactiontracker.org/countries/usa/2019-09-19/"
+      />
+      <Reference
+        id={7}
+        text="Climate Action Tracker. (2019, September 19). Climate Action Tracker | Canada. Retrieved November 28, 2020, from https://climateactiontracker.org/countries/canada/2019-09-19/"
+      />
+      <Reference
+        id={8}
+        text="Climate Action Tracker. (2019, September 19). Climate Action Tracker | Mexico. Retrieved November 28, 2020, from https://climateactiontracker.org/countries/mexico/2019-09-19/"
+      />
+      <Reference
+        id={9}
+        text="Climate Action Tracker. (2020, July 30). Climate Action Tracker | USA. Retrieved November 28, 2020, from https://climateactiontracker.org/countries/usa/"
+      />
+      <Reference
+        id={10}
+        text="Climate Action Tracker. (2020, September 21). Climate Action Tracker | Canada. Retrieved November 28, 2020, from https://climateactiontracker.org/countries/canada/"
+      />
+      <Reference
+        id={11}
+        text="Climate Action Tracker. (2020, September 22). Climate Action Tracker | Mexico. Retrieved November 28, 2020, from https://climateactiontracker.org/countries/mexico/"
+      />
     </Section>
   </PageContainer>
 );
