7e36496649ad0723ca944fb7acd1917f0e7b425f
Kirat <hrakhra@mail.uoguelph.ca>
2020-12-09 21:59:55 -0500

    created route to literature review page

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import LiteratureReivew from './literatureReview/LiteratureReview';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 const App = () => {
   const [testVal, setTestVal] = useState('');
@@ -13,6 +20,12 @@ const App = () => {
     <>
       <h1>CIS4020: Data Dashboard</h1>
       <p>Hello, World!</p>
+      <a href="/literature">temp link to literature Reviews</a>
+      <Router>
+        <Switch>
+          <Route path = "/literature" exact component={LiteratureReivew}/>
+        </Switch>
+      </Router>
       <code>{testVal}</code>
     </>
   );
---
+++ src/components/literatureReview/LiteratureReview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './LiteratureReview.css';
+
+class LiteratureReview extends React.Component{
+  render(){
+    return(
+      <div className = "LiteratureReview">
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+export default LiteratureReview;
